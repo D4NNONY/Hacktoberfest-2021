5217e085631f36df9d0e40b993854f374f603327
Matheus Cavalcanti <56284773+D4NNONY@users.noreply.github.com>
2020-10-30 23:36:27 -0300

    Update exc 3.py
    resolvi facin

--- exc 3.py
+++ exc 3.py
@@ -3,3 +3,11 @@ exc 3
 Receber do teclado uma mensagem e imprimir quantas
 letras A, E, I, O, U tem esta mensagem
 '''
+msg = input('digite uma mensagem qualquer:')
+vogais = 0
+vogais = vogais + msg.count('A') + msg.count('a')
+vogais = vogais + msg.count('E') + msg.count('e')
+vogais = vogais + msg.count('I') + msg.count('i')
+vogais = vogais + msg.count('O') + msg.count('o')
+vogais = vogais + msg.count('U') + msg.count('u')
+print('Há {} vogais nesta mensagem'.format(vogais))
