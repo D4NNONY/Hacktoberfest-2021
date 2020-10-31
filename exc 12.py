bba86ba10c2cbe26db628477dd0168ce87e05374
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-30 22:22:03 -0300

    Create exc 12.py

---
+++ exc 12.py
@@ -0,0 +1,8 @@
+'''
+exc 12
+Solicitar um nome e mostrar o primeiro, o último, o
+quarto, do primeiro ao terceiro, do primeiro ao ultimo
+caracter digitado e suas posições. Mostrar a posição de cada
+letra no vetor.Mostrar quantas letras tem o nome e escrevêlo de trás prá frente. Mostrar o número de vogais e
+constantes
+'''
