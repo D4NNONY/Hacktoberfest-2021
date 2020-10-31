9a08817efc3769cd76d6f46e60f3234d32018a23
Matheus Cavalcanti <56284773+D4NNONY@users.noreply.github.com>
2020-10-30 23:40:30 -0300

    Update exc 4.py
    moleza

--- exc 4.py
+++ exc 4.py
@@ -3,3 +3,6 @@ exc 4
 Receber um nome no teclado e imprimir quantas letras
 "A" tem o nome. 
 '''
+nome = input('digite um nome qualquer:')
+n = nome.count('A') + nome.count('a')
+print(nome+' tem {} letras A'.format(n))
