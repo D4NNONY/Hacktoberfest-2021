c8024b17b1f01568a2b0a051dbd5468d5db60fa9
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-30 22:20:26 -0300

    Create exc 9.py

---
+++ exc 9.py
@@ -0,0 +1,7 @@
+'''
+exc 9
+Solicitar no teclado uma frase com no máximo 40 letras.
+Se o tamanho for maior que 40, dar uma mensagem de
+entrada inválida e solicitar novamente, se passar, imprimir a
+frase na vertical com um tempo em cada letra.
+'''
