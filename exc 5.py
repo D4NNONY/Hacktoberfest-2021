857634ecc8af5d99cc1a9395141cac94dfc116f6
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-30 22:18:17 -0300

    Create exc 5.py

---
+++ exc 5.py
@@ -0,0 +1,15 @@
+'''
+exc 5
+Criar um algoritmo que entre com uma palavra e
+imprima conforme exemplo a seguir:
+Exemplo: SONHO
+Como a palavra SONHO tem 5 letras a impressão ficaria
+assim:
+SONHO
+SONHO SONHO
+SONHO SONHO SONHO
+SONHO SONHO SONHO SONHO
+SONHO SONHO SONHO SONHO SONHO
+Repare que foram impressos 5 vezes na horizontal e 5 na
+vertical
+'''
