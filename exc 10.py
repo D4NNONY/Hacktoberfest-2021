012ca10829d092b2ba169c2997cf7b2d7fcbbcfb
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-30 22:20:50 -0300

    Create exc 10.py

---
+++ exc 10.py
@@ -0,0 +1,10 @@
+'''
+exc 10
+Escrever um programa que receba um nome
+-Que conte o número de vogais existentes nele.
+-O programa deverá imprimir o número total de
+caracteres do nome
+-Quantas vogais
+- E a respectiva porcentagem das vogais em relação
+ao total de caracteres.
+'''
