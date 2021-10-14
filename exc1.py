0a96a35fe0596a82bdf87bc78f2871b5e5675a2e
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2021-10-13 23:09:22 -0300

    Update exc1.py

--- exc1.py
+++ exc1.py
@@ -4,3 +4,12 @@ Ler nome, sexo e idade. Se sexo for feminino e idade
 menor que 25. Imprimir o nome da pessoa e a palavra
 ACEITA. Caso contrario imprimir NAO ACEITA.
 '''
+
+nome = String(input("insira seu nome: "))
+sexo = String(input("insira seu sexo: "))
+idade = int(input("insira sua idade: "))
+
+if (sexo == "feminino" and idade < 25):
+  print("ACEITA")
+else:
+  print("NAO ACEITA")
