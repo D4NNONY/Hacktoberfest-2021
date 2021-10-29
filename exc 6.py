8d64b85615d398c70dd8f1509777fc8f87210531
NinjaPro93 <68875016+NinjaPro93@users.noreply.github.com>
2021-10-29 09:27:03 -0300

    Update exc 6.py

--- exc 6.py
+++ exc 6.py
@@ -3,3 +3,9 @@ exc 6
 Receber um nome do teclado e imprimí-lo de trás pra
 frente.
 '''
+word = input('digite a palavra e veja o que acontece: ')
+word = list(word)
+word.reverse()
+word = ''.join(word)
+print(word)
+input()
