6c8024de2b0f7a7399132c0a415fd5b3e4bc0f8d
LivSith <liviasithmendes@gmail.com>
2021-10-30 00:29:07 -0300

    Solucao exc 9.py

--- exc 9.py
+++ exc 9.py
@@ -5,3 +5,11 @@ Se o tamanho for maior que 40, dar uma mensagem de
 entrada inválida e solicitar novamente, se passar, imprimir a
 frase na vertical com um tempo em cada letra.
 '''
+
+frase = str(input("insira uma frase com no máximo 40 letras: "))
+
+while len(frase) > 40:
+    print(f'entrada inválida!')
+    frase = str(input("insira novamente: "))
+for letra in frase:
+        print(letra)
