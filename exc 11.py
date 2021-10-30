0f47f670a0099a756243822273a71bdfb1b02ae9
Samuel Oliveira <56443581+BOT-Samuel@users.noreply.github.com>
2021-10-30 20:45:01 -0300

    Update exc 11.py

--- exc 11.py
+++ exc 11.py
@@ -5,3 +5,7 @@ Receber um nome no teclado e imprimir a seguinte saída
 - Primeiro caracter:
 - Do primeiro até o terceiro caracter:
 '''
+nome = input("Digite seu nome: ")
+print("Esse é seu nome: ", nome)
+print("Primeiro caracter: ", nome[:1])
+print("Primeiro até o terceiro caracter: ", nome[:3])
