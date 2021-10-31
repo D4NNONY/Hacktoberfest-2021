382659f4f9a241c3306ef618bbb7747c00e8a57f
LivSith <liviasithmendes@gmail.com>
2021-10-30 22:08:42 -0300

    Add exercício exc 13.py

---
+++ exc 13.py
@@ -0,0 +1,8 @@
+''''
+Criar um algoritmo FizzBuzz que retorne:
+1. Se a posição for multipla de 3 fala "Fizz";
+2. Se a posição for multipla de 5 fala "Buzz"; 
+3. Se a posição não for múltipla de 3 nem de 5, fala a posição;
+4. E se a posição for multipla de 3 e 5 fala "FizzBuzz".
+
+'''
