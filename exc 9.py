f134f140ec5f6b980925427618ebfb59ebd7d862
josymarss <sylvajosymar@gmail.com>
2021-10-31 03:21:48 +0100

    Resolve exc9

--- exc 9.py
+++ exc 9.py
@@ -5,3 +5,12 @@ Se o tamanho for maior que 40, dar uma mensagem de
 entrada inválida e solicitar novamente, se passar, imprimir a
 frase na vertical com um tempo em cada letra.
 '''
+while True:
+      word=input("Escreva uma frase: \n")
+      if len(word) > 40:
+            print("entrada inválida \n")
+            continue
+      else:
+            for wo in word:
+                  print(wo,"\n")
+            break
